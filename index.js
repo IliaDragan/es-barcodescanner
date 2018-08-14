5b5faa077a105b3ec922ff28fe0ef86747d4a192
root <root@DESKTOP-J86MG6H.localdomain>
2018-08-14 11:39:46 +0300

    ES-1648 - Fix for cli config. Added main package exports.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-/* Server here */
+const { getScannerIdAndStatus, disableOutput, enableOutput } = require("./src/xinput");
+const { getScanner, getDeviceDevInputEvent } = require("./src/cat-devices");
+const { spawnEvtest } = require("./src/evtest");
+
+module.exports = {
+  getScannerIdAndStatus,
+  disableOutput,
+  enableOutput,
+  getScanner,
+  getDeviceDevInputEvent,
+  spawnEvtest
+};
