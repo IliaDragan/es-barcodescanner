1ed322512058d748283b4f5be203eda9fd4fe4c2
Anton Vashurkin <cronosfera2@yandex.ru>
2020-02-03 16:32:46 +0400

    ES-1937 - Added more known devices.

--- src/known-devices.js
+++ src/known-devices.js
@@ -1,21 +1,36 @@
 /* List of known devices for detecting app from cat-devices and xinput. */
-var knownDevices = [
-  {
-    name: "USB HJ ScannerApp",
-    vendor: "0483",
-    product: "5741"
-  },
-  {
-    name: "Future             USB Virtual PS2 Port    ",
-    vendor: "04b4",
-    product: "0100"
-  },
-  {
-    name: "Newland Auto-ID FM100 HID KBW",
-    vendor: "1eab",
-    product: "1603"
-  }
-];
+var knownDevices = [{
+  name: "USB HJ ScannerApp",
+  vendor: "0483",
+  product: "5741"
+},
+{
+  name: "Barcode Scanner 1.0.0",
+  manufacturer: "STMicroelectronics",
+  vendor: "0483",
+  product: "5740"
+},
+{
+  name: "Future             USB Virtual PS2 Port    ",
+  vendor: "04b4",
+  product: "0100"
+},
+{
+  name: "Newland Auto-ID FM100 HID KBW",
+  vendor: "1eab",
+  product: "1603"
+},
+{
+  name: "FM100",
+  vendor: "1EAB",
+  product: "0C06"
+},
+{
+  name: "Newland Auto-ID NLS-FM430-RC USB HID Keyboard",
+  vendor: "1eab",
+  product: "1d03"
+}];
+
 
 module.exports = {
   /* Direct comparison. Used in cat-devices. */
